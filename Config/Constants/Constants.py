b2df3c11a3e8ee79d1e7f361c698e401e6a9fe08
DmitriySolontsevoy <ling_w@mail.ru>
2019-04-29 12:15:24 +0300

    Slight fix

--- Config/Constants/Constants.py
+++ Config/Constants/Constants.py
@@ -5,6 +5,6 @@ class Constants:
     LOGIN_TAG_ID = "email"
     PASS_TAG_ID = "pass"
     BUTTON_ID = "u_0_2"
-    FRIEND_LIST_LINK = "https://www.facebook.com/profile.php?id=100035970048527&sk=friends&ft_ref=flsa"
+    FRIEND_LIST_LINK = "https://www.facebook.com/profile.php?sk=friends&ft_ref=flsa"
     FRIENDS_CONTAINER = "collection_wrapper_2356318349"
 
